9a20ed528b1766368cd05f0ee68f3e77381f112d
HuckOps <1059231721@qq.com>
2026-08-31 01:15:12 +0800

    ci: update lint config

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+
+module.exports = defineConfig([
+  expoConfig,
+
+  {
+    ignores: [
+      'dist/*',
+      'node_modules/*',
+      '.expo/*',
+      '.expo-shared/*',
+      'web-build/*',
+      'ios/*',
+      'android/*',
+    ],
+  },
+
+  {
+    rules: {
+      'react-hooks/set-state-in-effect': 'off',
+    },
+  },
+]);
